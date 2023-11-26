317570bc7c64cbb2e1b903161a1e68ff0e10d93c
Hadi Nawfal <86598039+HadiElNawfal@users.noreply.github.com>
2023-11-26 13:35:28 +0200

    Update Testing.py

--- Hadi/Testing.py
+++ Hadi/Testing.py
@@ -15,8 +15,8 @@ while True:
     ah_packet =  IP(src="192.168.1.1", dst="192.168.1.2") / AH()
     send_ipsec_packet(ah_packet)
 
-    time.sleep(1)
-
 # Fabricate and send packet with invalid IHL
-    #invalid_ihl_packet = Ether(dst="28:3A:4D:46:A2:49") / IP(src="192.168.1.1", dst="192.168.1.2", ihl=6) / UDP()
-    #send_ipsec_packet(invalid_ihl_packet)
+    invalid_ihl_packet =  IP(src="192.168.1.1", dst="192.168.1.2", ihl=6) / UDP()
+    send_ipsec_packet(invalid_ihl_packet)
+
+    time.sleep(1)
