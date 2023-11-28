38e1c59c15d27abfa771ff44e2da6f46aa06d1d2
Hadi Nawfal <86598039+HadiElNawfal@users.noreply.github.com>
2023-11-29 00:43:22 +0200

    Update Testing.py

--- Hadi/Testing.py
+++ Hadi/Testing.py
@@ -1,8 +1,8 @@
-from scapy.all import IP, UDP, Ether, ESP, AH, send
+from scapy.all import IP, UDP, Ether, ESP, AH, send, conf
 import time
 #-------------------------------------------------------------------------------------------by Hadi
 #this code is for testing IPSec code
-conf.iface = "Realtek RTL8822BE 802.11ac PCIe Adapter" #default interface
+conf.iface = "Realtek RTL8822BE 802.11ac PCIe Adapter" #default interface in case I remove src and dst
 #function to send IPSec packet
 def sendIPSecPacket(packet):
     send(packet, verbose=0)
@@ -10,15 +10,15 @@ def sendIPSecPacket(packet):
 while True:
 
     #fabricating and sending ESP packet 
-    espPacket = IP() / ESP()
-    sendIPsecPacket(espPacket)
+    espPacket = IP(src="192.168.1.1", dst="192.168.1.2") / ESP()
+    sendIPSecPacket(espPacket)
 
     #fabricating and sending AH packet
-    ahPacket =  IP() / AH()
-    sendIPsecPacket(ahPacket)
+    ahPacket =  IP(src="192.168.1.1", dst="192.168.1.2") / AH()
+    sendIPSecPacket(ahPacket)
 
     #fabricating and sending packet with invalid IHL
-    invalidIHLPacket =  IP(ihl=6) / UDP() #can be UDP or TCP
+    invalidIHLPacket =  IP(src="192.168.1.1", dst="192.168.1.2", ihl=6) / UDP() #can be UDP or TCP
     sendIPSecPacket(invalidIHLPacket)
 
     time.sleep(1) #time before sending more packets
