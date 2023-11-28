224db87c9d5b1ce2de6abf84f80512be6cbdab44
Hadi Nawfal <86598039+HadiElNawfal@users.noreply.github.com>
2023-11-28 22:41:46 +0200

    Update Comments

--- Hadi/IPSec.py
+++ Hadi/IPSec.py
@@ -1,45 +1,43 @@
 from scapy.all import IP, ESP, AH, sniff
-from packet_fabrication import send_ipsec_packet
-
-def detect_protocol(packet):
-    # Check if the packet is long enough to contain IP header
+from Testing import send_ipsec_packet
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------by Hadi
+#IPSec
+def detectProtocol(packet):
     if IP not in packet:
-        return "Invalid Packet: No IP Header"
+        return "Invalid Packet: No IP Header" #checks if packet contains an IP header; if not, it prints "Invalid Packet: No IP Header"
+    ipHeader = packet[IP] #extracts IP header
+    #checking the length of the IP header
+    ihl = ipHeader.ihl  #extracts internet header length
+    expectedHeaderLength = ihl * 4  #IHL is in 4-byte units
+    if len(ipHeader) < expectedHeaderLength:
+        return f"Invalid Packet: IP Header Length Mismatch (Expected: {expectedHeaderLength}, Actual: {len(ipHeader)})" #checks if the actual length of the IP header is less than the expected length; 
+                                                                                                                        #if it is, it returns an "Invalid Packet: IP Header Length Mismatch" message.
+    protocol = ipHeader.proto #extracts protocol number from the header
 
-    # Extract the IP header fields
-    ip_header = packet[IP]
-    # Check the length of the IP header
-    ihl = ip_header.ihl  # Internet Header Length
-    expected_header_length = ihl * 4  # IHL is in 4-byte units
-    if len(ip_header) < expected_header_length:
-        return f"Invalid Packet: IP Header Length Mismatch (Expected: {expected_header_length}, Actual: {len(ip_header)})"
-
-    protocol = ip_header.proto
-
-    # Check if it's an IP packet
-    if protocol == 50:  # Protocol number for IPsec ESP
+    #checing for IPSec
+    if protocol == 50:  #Protocol number for IPsec ESP
         return "IPsec ESP"
-    elif protocol == 51:  # Protocol number for IPsec AH
+    elif protocol == 51:  #Protocol number for IPsec AH
         return "IPsec AH"
-    elif protocol == 6:  # Protocol number for TCP
+    elif protocol == 6:  #Protocol number for TCP
         return "IP (with TCP)"
-    elif protocol == 17:  # Protocol number for UDP
+    elif protocol == 17:  #Protocol number for UDP
         return "IP (with UDP)"
     else:
         return f"IP (with Unknown Protocol {protocol})"
 
-def packet_handler(packet):
-    result = detect_protocol(packet)
+def packetHandler(packet):
+    result = detectProtocol(packet)
     print(f"Captured Packet: {result}")
 
-# Sniff packets from the network and apply the detection function
-def sniff_and_detect():
+#sniffing packets from the network
+def sniffPackets():
     sniff(prn=packet_handler)
 
-# Start sniffing in a separate thread
+#starting sniffing in a separate thread
 import threading
-sniff_thread = threading.Thread(target=sniff_and_detect)
-sniff_thread.start()
+sniffThread = threading.Thread(target=sniffPackets)
+sniffThread.start()
 
-# Keep the main thread running
-sniff_thread.join()
+#keeping the main thread running
+sniffThread.join()
