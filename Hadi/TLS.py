625757ed3fd0268849754b0a2c84b998099c1d42
Hadi Nawfal <86598039+HadiElNawfal@users.noreply.github.com>
2023-11-29 18:23:35 +0200

    Update TLS.py

--- Hadi/TLS.py
+++ Hadi/TLS.py
@@ -13,29 +13,30 @@ def checkSSLTLS(url): #takes a URL as input and checks various SSL/TLS features.
         httpsURL = 'https://' + url #constructing an HTTPS URL.
         responseHTTPS = requests.get(httpsURL, verify=False) #using requests.get to make an http GET request to the provided url with ssl verification disabled
         
-        if responseHTTPS.status_code == 200:#if the HTTPS response has a status code of 200, set the protocol to 'https'
+        if responseHTTPS.status_code == 200:#if the HTTPS response has a status code of 200, 
+                                            #set the protocol to 'https'
            
             urlProtocol = 'https'
         
         else:
-            print(f"The website {url} does not support HTTPS and is vulnerable to SSL stripping.")
+            print(f"The website {url} does not support HTTPS and is vulnerable to SSL stripping.") #else print it doesn't support https
             return
 
-        #check HSTS and cookies
-        if urlProtocol == 'https' and 'Strict-Transport-Security' in responseHTTPS.headers:
-            print(f"The website {httpsURL} has HSTS enabled.")
+        #check HSTS
+        if urlProtocol == 'https' and 'Strict-Transport-Security' in responseHTTPS.headers: #checks for HSTS (HTTPS Strict-Transport-Security) in https header
+            print(f"The website {httpsURL} has HSTS enabled.") #prints it has hsts
         elif urlProtocol == 'https':
-            print(f"The website {httpsURL} does not have HSTS enabled and might be vulnerable to SSL stripping.")#checks for Strict-Transport-Security in the header
+            print(f"The website {httpsURL} does not have HSTS enabled and might be vulnerable to SSL stripping.")#else prints website doesn't have hsts
         
         #creating a custom SSL context to get SSL/TLS information, this part is from chatgpt-------------------------------------------------------
         hostname = url.split('://', 1)[-1].split('/')[0] # Extract the hostname from the URL.
         sslContext = ssl.create_default_context() # Use ssl.create_default_context() to create a default SSL context
-        conn = sslContext.wrap_socket(socket.socket(socket.AF_INET), serverHostname=hostname) # Wrap the socket with the SSL context to create a secure socket.
+        conn = sslContext.wrap_socket(socket.socket(socket.AF_INET), server_hostname=hostname) # Wrap the socket with the SSL context to create a secure socket.
         conn.connect((hostname, 443)) # Establish a connection to the server using the SSL-wrapped socket.
             
         
-        print(f"Cipher suite: {conn.cipher()}")
-        print(f"SSL/TLS version: {conn.version()}")
+        print(f"Cipher suite: {conn.cipher()}")#prints cipher suite
+        print(f"SSL/TLS version: {conn.version()}")#prints TLS version
 #--------------------------------------------------------------------------------------------------------------------------------------------------      
         # Check for secure flag in cookies
         if 'Set-Cookie' in responseHTTPS.headers:
@@ -48,6 +49,6 @@ def checkSSLTLS(url): #takes a URL as input and checks various SSL/TLS features.
                 print("Warning: Some cookies do not have the 'Secure' flag.")#checks for Set-Cookie in the header
         
     except (requests.exceptions.RequestException, SSLError) as e:
-        print(f"Error: {e}")
+        print(f"Error: {e}")#handles errors
 
-checkSSLTLS('www.site.idk')#need to still parse this
+checkSSLTLS('www.socialstudieshelp.com/')#takes website as input
